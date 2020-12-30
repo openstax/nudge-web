f94dcfc88261f43a402ebdc040d8b0cbaf2a8a6b
Steve Burkett <steve@burkettnet.com>
2020-12-30 15:45:40 -0800

    Add avro-turf to the mix for decoding the avro

--- README.md
+++ README.md
@@ -8,4 +8,9 @@ This tiny web server consists of two parts:
 group and writes these json files
 2) a sinatra endpoint that fetches them by /user_uuid/book_uuid
 
+Each JSON file looks like this
+```bigquery
+[ruby-2.4.5](git:):tmp $ cat ac27f48d-8a54-49ad-a361-cab9c09e8181_37323462-3861-3339-2d66-6236362d3464.json 
+{"device_uuid":"6a814ffb-d650-4ad7-89e8-bc13ecfd3e35","user_uuid":"ac27f48d-8a54-49ad-a361-cab9c09e8181","session_uuid":"6bf48c7a-5973-4b8f-a4c5-a85b72ad0186","session_order":1,"app":"green cows","target":"study_guides","context":"37323462-3861-3339-2d66-6236362d3464","flavor":"full-screen-v2","medium":"in-app","occurred_at":"1970-01-19 15:02:51 UTC"}
+```
 
--- nudge_web_writer.rb
+++ nudge_web_writer.rb
@@ -1,34 +1,72 @@
 require 'daemons'
 require 'ruby-kafka'
+require 'avro_turf'
+require 'avro_turf/messaging'
 require 'json'
 
 # Process.daemon()
 
-file_shared_volume = '/tmp/'
-kafka = Kafka.new(['localhost:9092'])
-consumer = kafka.consumer(group_id: 'nudged-consumer')
-consumer.subscribe('nudged')
+COMPACT_FORM = /(\w{8})(\w{4})(\w{4})(\w{4})(\w{12})/i
+FILE_SHARED_VOLUME = '/tmp/'
+SCHEMA_REGISTRY_URL = 'http://localhost:8081/'
+NUDGED_SCHEMA_NAME = 'org.openstax.ec.nudged_v1'
 
-trap("TERM") { consumer.stop }
-trap("SIGINT") { exit! }
+trap('TERM') do
+  kafka_consumer.stop
+end
+trap('SIGINT') do
+  kafka_consumer.stop
+  exit!
+end
+
+def unpack(packed_uuid)
+  unpacked_nohypens = packed_uuid.unpack1('H*')
+  parts = unpacked_nohypens.match(COMPACT_FORM)
+  parts.captures.join('-')
+rescue
+  "Unable to unpack #{packed_uuid}"
+end
+
+def kafka_consumer
+  @kafka_consumer ||= begin
+    consumer = kafka.consumer(group_id: 'nudged-consumer')
+    consumer.subscribe('nudged')
+    consumer
+  end
+end
+
+def kafka
+  @kafka ||= Kafka.new(['localhost:9092'])
+end
+
+def avroturf
+  @avro ||= AvroTurf::Messaging.new(registry_url: SCHEMA_REGISTRY_URL)
+end
 
 loop do
   begin
-    consumer.each_message() do |message|
-      puts "Reading message from consumer: #{message.inspect}"
-      # determine user and book uuids from message
-      user_uuid = 'foo'
-      book_uuid = 'bar'
-      file_name = file_shared_volume + "#{user_uuid}_#{book_uuid}.json"
+    kafka_consumer.each_message() do |message|
+      begin
+        nudge = avroturf.decode(message.value, schema_name: NUDGED_SCHEMA_NAME)
 
-      File.open(file_name, 'a') do |file|
-        file.puts(message.to_json)
+        # puts "Reading message from consumer: #{nudge.inspect}"
+        nudge['device_uuid'] = unpack(nudge['device_uuid'])
+        nudge['session_uuid'] = unpack(nudge['session_uuid'])
+        user_uuid = nudge['user_uuid'] = unpack(nudge['user_uuid'])
+        book_uuid = nudge['context'] = unpack(nudge['context'])
+        file_name = FILE_SHARED_VOLUME + "#{user_uuid}_#{book_uuid}.json"
+
+        puts "Opening file #{file_name} for content #{nudge.to_s}"
+        File.open(file_name, 'a') do |file|
+          file.puts(nudge.to_json)
+        end
+      rescue => ex
+        puts ex.inspect
+        next
       end
     end
   rescue Kafka::ProcessingError => ex
-    puts ex
-    puts ex.cause
-    retry
+    puts ex.cause.inspect
   end
   # sleep(10)  # 10 second breather
 end
